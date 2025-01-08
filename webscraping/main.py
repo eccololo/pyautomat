fee6c96c381aee16147a194bd13e735f59e8aff4
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-01-08 14:52:25 +0100

    Refactor code.

--- webscraping/main.py
+++ webscraping/main.py
@@ -1,35 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-import os
+from settings import *
+from utils import *
 
 
-WEBDRIVER_PATH = os.path.join(os.getcwd(), "webscraping", "chromedriver.exe")  
 
 service = Service(WEBDRIVER_PATH)
 
 def get_driver(target_url):
-    options = webdriver.ChromeOptions()
-
-    # ============== SET OPTIONS TO MAKE BROWSING EASIER ============
-
-    # Disable showing bars with additional info.
-    options.add_argument("disable-infobars")
-
-    options.add_argument("start-maximized")
-
-    # Avoiding issued when using Linux machine.
-    options.add_argument("disable-dev-shm-usage")
-
-    #Gives greater priviliges to browser.
-    options.add_argument("no-sandbox")
-
-    # Options that help Selenium avoid detection from the browser.
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_argument("disable-blink-features=AutomationControlled")
-
-    # ============= OPTIONS ENDS =============
-
+    options = set_driver_options(webdriver)
     driver = webdriver.Chrome(service=service, options=options)
     driver.get(target_url)
 
---
+++ webscraping/utils.py
@@ -0,0 +1,19 @@
+def set_driver_options(webdriver):
+    options = webdriver.ChromeOptions()
+
+    # Disable showing bars with additional info.
+    options.add_argument("disable-infobars")
+
+    options.add_argument("start-maximized")
+
+    # Avoiding issued when using Linux machine.
+    options.add_argument("disable-dev-shm-usage")
+
+    #Gives greater priviliges to browser.
+    options.add_argument("no-sandbox")
+
+    # Options that help Selenium avoid detection from the browser.
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
+    
+    return options
